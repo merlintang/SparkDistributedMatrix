5ef8142cd58ecf4f72c2be67f9a74ca9967a606d
Mingjie Tang <mtang@hortonworks.com>
2017-08-17 17:21:55 -0700

    update with Apache License

--- src/main/scala/org/apache/spark/sql/matfast/matrix/MLMatrix.scala
+++ src/main/scala/org/apache/spark/sql/matfast/matrix/MLMatrix.scala
@@ -1,4 +1,3 @@
-package org.apache.spark.sql.matfast.matrix
 /*
  * Licensed to the Apache Software Foundation (ASF) under one or more
  * contributor license agreements.  See the NOTICE file distributed with
@@ -15,6 +14,8 @@ package org.apache.spark.sql.matfast.matrix
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
+package org.apache.spark.sql.matfast.matrix
 
 import java.util.{Arrays, Random}
 
